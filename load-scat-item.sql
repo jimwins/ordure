22f76a43752c87b0fe7e4f783554fcdbe65f4525
jim winstead <jimw@trainedmonkey.com>
2013-11-23 12:35:28 -0800

    Set stock quantity to NULL for inactive products

--- load-scat-item.sql
+++ load-scat-item.sql
@@ -3,7 +3,7 @@ TRUNCATE scat_item;
 INSERT INTO scat_item
        (code, retail_price, discount_type, discount, stock)
 SELECT code, retail_price, discount_type, discount,
-       IFNULL((SELECT SUM(allocated)
-                 FROM scat.txn_line
-                WHERE item = scat.item.id), 0) stock
+       IF(active,
+          (SELECT SUM(allocated) FROM scat.txn_line WHERE item = scat.item.id),
+          NULL) stock
   FROM scat.item;
