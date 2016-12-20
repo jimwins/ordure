e1bae4e3c81ed418daa7ea7a20f8927053ae653f
jim winstead <jimw@trainedmonkey.com>
2016-12-19 17:45:20 -0800

    Fix override of retail price and purchase quantity by scat_item

--- lib/sale.php
+++ lib/sale.php
@@ -184,18 +184,18 @@ class Sale {
       or $f3->error(404);
 
     $item= new DB\SQL\Mapper($db, 'item');
-    $item->retail_price= "IFNULL((SELECT retail_price FROM scat_item WHERE scat_item.code = item.code), retail_price)";
+    $item->nretail_price= "IFNULL((SELECT retail_price FROM scat_item WHERE scat_item.code = item.code), retail_price)";
     $item->discount_type= "(SELECT discount_type FROM scat_item WHERE scat_item.code = item.code)";
     $item->discount= "(SELECT discount FROM scat_item WHERE scat_item.code = item.code)";
-    $item->purchase_quantity= "IFNULL((SELECT purchase_quantity FROM scat_item WHERE scat_item.code = item.code), purchase_quantity)";
+    $item->npurchase_quantity= "IFNULL((SELECT scat_item.purchase_quantity FROM scat_item WHERE scat_item.code = item.code), purchase_quantity)";
     $item->load(array('code = ?', $item_code))
       or $f3->error(404);
 
     $line= new DB\SQL\Mapper($db, 'sale_item');
     $line->sale_id= $sale->id;
     $line->item_id= $item->id;
-    $line->quantity= $item->purchase_quantity;
-    $line->retail_price= $item->retail_price;
+    $line->quantity= $item->npurchase_quantity;
+    $line->retail_price= $item->nretail_price;
     $line->discount_type= $item->discount_type;
     $line->discount= $item->discount;
     $line->discount_manual= 0;
