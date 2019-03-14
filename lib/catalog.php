cfa5c8de9d21496b641f18b558c5fcba97b3d8c0
jim winstead <jimw@trainedmonkey.com>
2019-03-13 21:15:31 -0700

    Set Content-type of sitemap.xml

--- lib/catalog.php
+++ lib/catalog.php
@@ -395,6 +395,7 @@ class Catalog {
 
     $urls= $db->exec($q);
 
+    header("Content-type: application/xml");
     echo '<?xml version="1.0" encoding="UTF-8"?>', "\n",
          '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">', "\n";
     foreach ($urls as $url) {
