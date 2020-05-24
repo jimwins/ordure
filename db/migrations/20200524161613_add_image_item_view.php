03a13c07adc372702374107b18757a5b37bcd7df
jim winstead <jimw@trainedmonkey.com>
2020-05-24 09:32:00 -0700

    Actually use variable to get database name

--- db/migrations/20200524161613_add_image_item_view.php
+++ db/migrations/20200524161613_add_image_item_view.php
@@ -7,7 +7,7 @@ class AddImageItemView extends AbstractMigration
     public function up()
     {
       $scat= $_ENV['SCAT_DATABASE'] ?: 'scat';
-      $this->execute("CREATE VIEW item_to_image AS SELECT * FROM scat.item_to_image");
+      $this->execute("CREATE VIEW item_to_image AS SELECT * FROM $scat.item_to_image");
 
     }
 
