3c653f65c0cb6227644e50f387c60096776f3173
jim winstead <jimw@trainedmonkey.com>
2020-05-24 18:18:43 -0700

    Fix login

--- lib/auth.php
+++ lib/auth.php
@@ -202,14 +202,14 @@ class Auth {
           $cart->name= $person['name'];
           $cart->save();
 
-          Sale::remember_cart($f3, $cart->uuid);
+          $cart->remember_cart($f3, $cart->uuid);
         }
       } else {
         $cart= new DB\SQL\Mapper($db, 'sale');
         $cart->load(array("person_id = ? AND status = 'cart'", $person['id']));
         if (!$cart->dry()) {
           error_log("Loading latest open cart {$cart->uuid}\n");
-          Sale::remember_cart($f3, $cart->uuid);
+          $cart->remember_cart($f3, $cart->uuid);
         }
       }
 
