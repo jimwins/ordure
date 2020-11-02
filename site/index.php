73ffd60b6bf1af54b1c13534153efd2b38f094d7
jim winstead <jimw@trainedmonkey.com>
2020-11-01 20:43:00 -0800

    Remove commented-out code

--- site/index.php
+++ site/index.php
@@ -440,30 +440,9 @@ $f3->route('GET|POST /~webhook/amazon', function ($f3) {
 
   error_log(json_encode($data)."\n");
 
-/*
-  if ($data['ObjectType'] == 'Notification') {
-    $sale= new Sale();
+  if ($data['NotificationType'] == 'PaymentCapture') {
 
-    // this is so dumb.
-    foreach ($data->resource->links as $link) {
-      if ($link->rel == 'up') {
-        $order_href= $link->href;
-      }
-    }
-
-    $order_id= basename(parse_url($order_href, PHP_URL_PATH));
-
-    $client= $sale->get_paypal_client($f3);
-
-    $response= $client->execute(
-      new \PayPalCheckoutSdk\Orders\OrdersGetRequest($order_id)
-    );
-
-    $uuid= $response->result->purchase_units[0]->reference_id;
-
-    return $sale->handle_paypal_payment($f3, $uuid, $order_id);
   }
-*/
 });
 
 $f3->route('GET|POST /~webhook/sandbox-amazon', function ($f3) {
